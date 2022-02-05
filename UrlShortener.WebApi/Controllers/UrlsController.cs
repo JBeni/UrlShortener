2aa0267d40a1af0aa0b59073522a1daa6cbd33c4
Jitca Beniamin <JBeni@users.noreply.github.com>
2022-02-05 11:53:06 +0200

    re-change folder name

---
+++ UrlShortener.WebApi/Controllers/UrlsController.cs
@@ -0,0 +1,47 @@
+﻿namespace UrlShortener.WebApi.Controllers
+{
+    public class UrlsController : ApiControllerBase
+    {
+        private readonly IUrlService _urlService;
+
+        public UrlsController(IUrlService urlService)
+        {
+            _urlService = urlService;
+        }
+
+        [HttpPost("post")]
+        public async Task<IActionResult> CreatePost([FromBody] CreatePostCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPut("post")]
+        public async Task<IActionResult> UpdatePost([FromBody] UpdatePostCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpDelete("post/{id}/{userId}")]
+        public async Task<IActionResult> DeletePost(int id, int userId)
+        {
+            var result = await Mediator.Send(new DeletePostCommand { Id = id });
+            return Ok(result);
+        }
+
+        [HttpGet("post/{id}")]
+        public async Task<IActionResult> GetPost(int id)
+        {
+            var result = await Mediator.Send(new GetPostByIdQuery { Id = id });
+            return Ok(result);
+        }
+
+        [HttpGet("posts/{userId}")]
+        public async Task<IActionResult> GetPosts(int userId)
+        {
+            var result = await Mediator.Send(new GetPostsQuery { });
+            return Ok(result);
+        }
+    }
+}
